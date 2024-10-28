7349736aa217fd81f7b21cf9415103292ef8ad13
Aadi Ahlawat <433586@conneqtcorp.com>
2024-10-28 12:48:34 +0530

    Implementing eds spreadsheet

--- blocks/spreadsheet/spreadsheet.js
+++ blocks/spreadsheet/spreadsheet.js
@@ -1,5 +1,5 @@
 export default async function decorate(block) {
-    const userList = block.querySelector('#user-list');
+    const usersList = block.querySelector('#users-list');
 
     try {
         const response = await fetch('https://develop--eds-tr--aadiahlawat.aem.page/eds-spreadsheet.json'); // Replace with your JSON URL
@@ -12,7 +12,7 @@ export default async function decorate(block) {
         data.data.forEach(user => {
             const li = document.createElement('li');
             li.textContent = user.name; // Adjust according to your JSON structure
-            userList.appendChild(li);
+            usersList.appendChild(li);
         });
     } catch (error) {
         console.error('Fetch error:', error);
