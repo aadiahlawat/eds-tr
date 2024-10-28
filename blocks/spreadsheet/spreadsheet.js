348cfb71059ccecc3189fc329ce6d7ffbc15e7fa
Aadi Ahlawat <433586@conneqtcorp.com>
2024-10-28 12:58:21 +0530

    Implementing eds spreadsheet

--- blocks/spreadsheet/spreadsheet.js
+++ blocks/spreadsheet/spreadsheet.js
@@ -11,6 +11,7 @@ export default async function decorate(block) {
         // Render the list users
         data.data.forEach(user => {
             const li = document.createElement('li');
+            console.log('User Data: '+user);
             li.textContent = user.name; // Adjust according to your JSON structure
             ul.appendChild(li);
         });
